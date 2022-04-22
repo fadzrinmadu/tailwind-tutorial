c98ff9cecb0679aec2d47ee0c74fcc245f9c0e92
Muhammad Fadzrin Madu <muhfadzrinmadu@gmail.com>
2022-04-23 02:38:19 +0700

    Basic utility bag.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '13': '3.25rem',
+        '15': '3.75rem',
+        '128': '32rem',
+        '144': '36rem',
+      },
+      fontFamily: {
+        inter: ['Inter'],
+      },
+      colors: {
+        refc: '#bada55',
+        coffee: '#c0ffee',
+      },
+    },
   },
   plugins: [],
 }
