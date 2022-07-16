2902497776d8a6851264f910750b5ea4f85cd1c7
Muhammad Fadzrin Madu <muhfadzrinmadu@gmail.com>
2022-07-16 19:40:07 +0700

    tranform, transition, animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,16 @@ module.exports = {
         refc: '#bada55',
         coffee: '#c0ffee',
       },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+      },
     },
   },
   darkMode: 'class',
